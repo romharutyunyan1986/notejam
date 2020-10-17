6ba66590ef03fcacaaa249877218b4ef324a826c
romharutyunyan1986 <63472885+romharutyunyan1986@users.noreply.github.com>
2020-10-17 23:05:50 +0400

    Update settings.py

--- django/notejam/notejam/settings.py
+++ django/notejam/notejam/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         'NAME': 'django_db',
         'USER': 'db_user',
         'PASSWORD': 'Passw0rd',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
