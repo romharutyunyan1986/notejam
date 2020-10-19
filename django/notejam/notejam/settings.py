123bf30288409f220c4ea1da2ae6c61eb0e7d339
romharutyunyan1986 <63472885+romharutyunyan1986@users.noreply.github.com>
2020-10-19 22:23:33 +0400

    Update settings.py

--- django/notejam/notejam/settings.py
+++ django/notejam/notejam/settings.py
@@ -11,7 +11,7 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -33,6 +33,7 @@ DATABASES = {
         'PORT': '',
     }
 }
+"""
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
